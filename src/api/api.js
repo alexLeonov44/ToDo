69ba05417d3846e4a1f929293d6c00deec6846b2
Alexander <leonovfx.91@gmail.com>
2022-02-26 20:35:00 +0300

    comment secure policy

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL:'http://api.openweathermap.org/data/2.5/',
+    baseURL:'https://api.openweathermap.org/data/2.5/',
     // withCredentials:true,
     // headers: {'appid':'f8e74434dec1483a8e7d3b0f9aa47d34'}
   });
